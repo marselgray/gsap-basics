b31a1b49d8ce47a2b2c042c493dd7178951ac6cd
Marsel Gray <marselgray@gmail.com>
2022-04-26 15:39:13 -0400

    Lesson: the basics of greensock the easing with gsap

--- app.js
+++ app.js
@@ -2,8 +2,4 @@ console.log(gsap);
 
 const img1 = document.querySelector('.img1');
 
-// gsap.from(img1, {autoAlpha: 0, duration: 2})
-
-document.querySelector('.btn').addEventListener('click', () => {
-    gsap.from(img1, {autoAlpha: 0, duration: 2})
-})
+gsap.from(img1, {autoAlpha: 0, y: -100, rotation: 90, ease: 'bounce', duration: 2});
